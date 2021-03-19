f9a65f33abe6a5214edea29c228912338cbf7636
priska19 <priska360@gmail.com>
2021-03-19 15:16:43 +0700

    Tugas 1 - Layout

---
+++ Tugas1Layout/app/src/main/java/com/example/tugas1layout/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tugas1layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText Pada, Subject, Pesan;
+    Button Submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.relative_layout);
+        //setContentView(R.layout.constraint_layout);
+        //setContentView(R.layout.frame_layout);
+        setContentView(R.layout.table_layout);
+
+
+
+
+        Pada = findViewById(R.id.pada);
+        Subject = findViewById(R.id.subject);
+        Submit = findViewById(R.id.submit);
+    }
+}
