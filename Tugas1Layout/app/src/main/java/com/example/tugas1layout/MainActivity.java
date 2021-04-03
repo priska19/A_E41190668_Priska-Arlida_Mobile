736b21b1db75fc6939a767fccfd46c3ce76b5282
priska19 <priska360@gmail.com>
2021-04-03 22:48:53 +0700

    Tugas minggu 3

--- Tugas1Layout/app/src/main/java/com/example/tugas1layout/MainActivity.java
+++ Tugas1Layout/app/src/main/java/com/example/tugas1layout/MainActivity.java
@@ -16,13 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         //setContentView(R.layout.relative_layout);
-        //setContentView(R.layout.constraint_layout);
+        setContentView(R.layout.constraint_layout);
         //setContentView(R.layout.frame_layout);
         //setContentView(R.layout.table_layout);
         //setContentView(R.layout.material_design);
         //setContentView(R.layout.scrollview);
         //setContentView(R.layout.scrollviewhorizontal);
-        setContentView(R.layout.latihan);
+        //setContentView(R.layout.latihan);
 
         Pada = findViewById(R.id.pada);
         Subject = findViewById(R.id.subject);
